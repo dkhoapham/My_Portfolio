71e988d66b47b0d294db8ed12a3de6466115ad22
kevin_da_dino <pham.dangkhoa.se@gmail.com>
2026-01-12 21:53:11 -0800

    Feat: adding, and style resume page.

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,13 +1,29 @@
+import "./resume.css";
+import { FaDownload } from "react-icons/fa";
+
 function Resume() {
+  const resumeViewLink =
+    "https://drive.google.com/file/d/1AlzKkw0Y1pcZjp7jNk8hpJtXPfqqnrjG/preview";
+  const resumeDownLoadLink =
+    "https://drive.google.com/uc?export=download&id=1AlzKkw0Y1pcZjp7jNk8hpJtXPfqqnrjG";
+
   return (
-    <>
-      <h1>This is my resume</h1>
-      <iframe
-        src="https://drive.google.com/file/d/1AlzKkw0Y1pcZjp7jNk8hpJtXPfqqnrjG/preview"
-        width="500"
-        height="600"
-      ></iframe>
-    </>
+    <div className="bodyResume">
+      <h1 className="titleResume">{`My Resume 🫡`}</h1>
+      <button
+        className="downloadBtnResume"
+        onClick={() => {
+          const a = document.createElement("a");
+          a.href = resumeDownLoadLink;
+          a.download = "Khoa's_Resume.pdf";
+          a.click();
+        }}
+      >
+        {`Download `}
+        <FaDownload className="downloadIcon" />
+      </button>
+      <iframe className="frameResume" src={resumeViewLink} />
+    </div>
   );
 }
 
